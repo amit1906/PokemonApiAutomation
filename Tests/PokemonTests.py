9156fc1db51e13f1f89db5fa946c70fc65a46eea
Amit <1amitg@walla.com>
2024-02-01 14:13:57 +0200

    update test_3_top_5_fire_type_weight to query all pokemons in type of fire

--- Tests/PokemonTests.py
+++ Tests/PokemonTests.py
@@ -21,16 +21,21 @@ def test_2_fire_type_pokemons():
     AssertionUtils.validate_not_in_list('bulbasaur', fire_pokemon_names, 'pokemon name')
 
 
-def test_3_fire_type_weight():
+def test_3_top_5_fire_type_weight():
     try:
         with open('../ExpectedFiles/ExpectedTop5WeightsPokemons.json', 'r') as file:
             expected_top_5_weights = json.load(file)
-        expected_pokemon_weights = list(zip([list(p.keys())[0] for p in expected_top_5_weights],
-                                            [list(list(p.values())[0].values())[0] for p in expected_top_5_weights]))
-        pokemon_names = [list(pokemon.keys())[0] for pokemon in expected_top_5_weights]
+        expected_pokemon_weights = [{name: value} for name, value in
+                                    zip([list(p.keys())[0] for p in expected_top_5_weights],
+                                        [list(list(p.values())[0].values())[0] for p in expected_top_5_weights])]
     except:
         raise Exception('Failed to extract ExpectedTop5WeightsPokemons.json file.')
 
-    actual_pokemon_weights = list(zip([pokemon_name for pokemon_name in pokemon_names],
-                                      [PokemonRequests.get_pokemon(p).get('weight') for p in pokemon_names]))
-    AssertionUtils.assert_equal(expected_pokemon_weights, actual_pokemon_weights, 'top 5 pokemon weights')
+    fire_types = PokemonRequests.get_pokemon_types('fire')
+    fire_pokemon_names = AssertionUtils.validate_jsonpath(fire_types, 'pokemon..pokemon.name')
+    actual_pokemon_weights = [{name: value} for name, value in
+                              zip([pokemon_name for pokemon_name in fire_pokemon_names],
+                                  [PokemonRequests.get_pokemon(p).get('weight') for p in fire_pokemon_names])]
+    actual_top_5_pokemon_weights = list(
+        sorted(actual_pokemon_weights, key=lambda p: list(p.values())[0], reverse=True))[:5]
+    AssertionUtils.assert_equal(expected_pokemon_weights, actual_top_5_pokemon_weights, 'top 5 pokemon weights')
---
+++ readme.txt
@@ -0,0 +1,5 @@
+
+- At test_3_top_5_fire_type_weight i assume there's no endpoint to retreive all pokemons of type 'fire' at once
+  thus i make requests 'pokemon' to each one of the pokemon names i retreived by type 'fire',
+  so i can get top 5 pokemons by ther'e weights.
+  This amount of requests make the test last about 20 seconds.
